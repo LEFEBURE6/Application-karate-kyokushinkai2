3fa281e0e773631adbe5583366c6fc4bf8c66491
LEFEBURE6 <flefebure@live.fr>
2026-03-12 15:10:23 +0100

    update

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
+// ➜ Route GET pour tester ton backend
+app.get("/", (req, res) => {
+  res.send("Backend opérationnel ✔️");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Serveur démarré sur le port ${PORT}`));
+
 
